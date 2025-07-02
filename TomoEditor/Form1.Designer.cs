14dc1a1f8132ccfbb5c57605559e46a59c2c8723
KushinaAtori <cjacksonsheppard@yahoo.com>
2025-07-02 15:09:03 -0500

    Inital Design

--- TomoEditor/Form1.Designer.cs
+++ TomoEditor/Form1.Designer.cs
@@ -28,12 +28,259 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges15 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges16 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges17 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges18 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges19 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges20 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges21 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges22 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges23 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges24 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges25 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges26 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges13 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges14 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            guna2TabControl1 = new Guna.UI2.WinForms.Guna2TabControl();
+            tabFTPSettings = new TabPage();
+            btnBack = new Guna.UI2.WinForms.Guna2Button();
+            btnRoot = new Guna.UI2.WinForms.Guna2Button();
+            btnDownload = new Guna.UI2.WinForms.Guna2Button();
+            btnSaveConnection = new Guna.UI2.WinForms.Guna2Button();
+            btnLoadConnection = new Guna.UI2.WinForms.Guna2Button();
+            btnNewConection = new Guna.UI2.WinForms.Guna2Button();
+            lstView = new ListView();
+            tabModifiers = new TabPage();
+            tabSettings = new TabPage();
+            guna2GroupBox1 = new Guna.UI2.WinForms.Guna2GroupBox();
+            guna2TabControl1.SuspendLayout();
+            tabFTPSettings.SuspendLayout();
+            tabModifiers.SuspendLayout();
+            SuspendLayout();
+            // 
+            // guna2TabControl1
+            // 
+            guna2TabControl1.Alignment = TabAlignment.Left;
+            guna2TabControl1.Controls.Add(tabFTPSettings);
+            guna2TabControl1.Controls.Add(tabModifiers);
+            guna2TabControl1.Controls.Add(tabSettings);
+            guna2TabControl1.Dock = DockStyle.Fill;
+            guna2TabControl1.ItemSize = new Size(180, 40);
+            guna2TabControl1.Location = new Point(0, 0);
+            guna2TabControl1.Name = "guna2TabControl1";
+            guna2TabControl1.SelectedIndex = 0;
+            guna2TabControl1.Size = new Size(800, 450);
+            guna2TabControl1.TabButtonHoverState.BorderColor = Color.Empty;
+            guna2TabControl1.TabButtonHoverState.FillColor = Color.FromArgb(40, 52, 70);
+            guna2TabControl1.TabButtonHoverState.Font = new Font("Segoe UI Semibold", 10F);
+            guna2TabControl1.TabButtonHoverState.ForeColor = Color.White;
+            guna2TabControl1.TabButtonHoverState.InnerColor = Color.FromArgb(40, 52, 70);
+            guna2TabControl1.TabButtonIdleState.BorderColor = Color.Empty;
+            guna2TabControl1.TabButtonIdleState.FillColor = Color.FromArgb(33, 42, 57);
+            guna2TabControl1.TabButtonIdleState.Font = new Font("Segoe UI Semibold", 10F);
+            guna2TabControl1.TabButtonIdleState.ForeColor = Color.FromArgb(156, 160, 167);
+            guna2TabControl1.TabButtonIdleState.InnerColor = Color.FromArgb(33, 42, 57);
+            guna2TabControl1.TabButtonSelectedState.BorderColor = Color.Empty;
+            guna2TabControl1.TabButtonSelectedState.FillColor = Color.FromArgb(29, 37, 49);
+            guna2TabControl1.TabButtonSelectedState.Font = new Font("Segoe UI Semibold", 10F);
+            guna2TabControl1.TabButtonSelectedState.ForeColor = Color.White;
+            guna2TabControl1.TabButtonSelectedState.InnerColor = Color.FromArgb(76, 132, 255);
+            guna2TabControl1.TabButtonSize = new Size(180, 40);
+            guna2TabControl1.TabIndex = 0;
+            guna2TabControl1.TabMenuBackColor = Color.FromArgb(33, 42, 57);
+            // 
+            // tabFTPSettings
+            // 
+            tabFTPSettings.Controls.Add(btnBack);
+            tabFTPSettings.Controls.Add(btnRoot);
+            tabFTPSettings.Controls.Add(btnDownload);
+            tabFTPSettings.Controls.Add(btnSaveConnection);
+            tabFTPSettings.Controls.Add(btnLoadConnection);
+            tabFTPSettings.Controls.Add(btnNewConection);
+            tabFTPSettings.Controls.Add(lstView);
+            tabFTPSettings.Location = new Point(184, 4);
+            tabFTPSettings.Name = "tabFTPSettings";
+            tabFTPSettings.Padding = new Padding(3);
+            tabFTPSettings.Size = new Size(612, 442);
+            tabFTPSettings.TabIndex = 0;
+            tabFTPSettings.Text = "3DS FTP";
+            tabFTPSettings.UseVisualStyleBackColor = true;
+            // 
+            // btnBack
+            // 
+            btnBack.BorderRadius = 4;
+            btnBack.CustomizableEdges = customizableEdges15;
+            btnBack.DisabledState.BorderColor = Color.DarkGray;
+            btnBack.DisabledState.CustomBorderColor = Color.DarkGray;
+            btnBack.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            btnBack.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            btnBack.Font = new Font("Segoe UI", 9F);
+            btnBack.ForeColor = Color.White;
+            btnBack.Location = new Point(463, 290);
+            btnBack.Name = "btnBack";
+            btnBack.ShadowDecoration.CustomizableEdges = customizableEdges16;
+            btnBack.Size = new Size(146, 37);
+            btnBack.TabIndex = 6;
+            btnBack.Text = "Back";
+            btnBack.Click += btnBack_Click;
+            // 
+            // btnRoot
+            // 
+            btnRoot.BorderRadius = 4;
+            btnRoot.CustomizableEdges = customizableEdges17;
+            btnRoot.DisabledState.BorderColor = Color.DarkGray;
+            btnRoot.DisabledState.CustomBorderColor = Color.DarkGray;
+            btnRoot.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            btnRoot.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            btnRoot.Font = new Font("Segoe UI", 9F);
+            btnRoot.ForeColor = Color.White;
+            btnRoot.Location = new Point(463, 247);
+            btnRoot.Name = "btnRoot";
+            btnRoot.ShadowDecoration.CustomizableEdges = customizableEdges18;
+            btnRoot.Size = new Size(146, 37);
+            btnRoot.TabIndex = 5;
+            btnRoot.Text = "Root /";
+            btnRoot.Click += btnRoot_Click;
+            // 
+            // btnDownload
+            // 
+            btnDownload.BorderRadius = 4;
+            btnDownload.CustomizableEdges = customizableEdges19;
+            btnDownload.DisabledState.BorderColor = Color.DarkGray;
+            btnDownload.DisabledState.CustomBorderColor = Color.DarkGray;
+            btnDownload.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            btnDownload.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            btnDownload.Font = new Font("Segoe UI", 9F);
+            btnDownload.ForeColor = Color.White;
+            btnDownload.Location = new Point(210, 247);
+            btnDownload.Name = "btnDownload";
+            btnDownload.ShadowDecoration.CustomizableEdges = customizableEdges20;
+            btnDownload.Size = new Size(173, 37);
+            btnDownload.TabIndex = 4;
+            btnDownload.Text = "Download";
+            btnDownload.Click += btnDownload_Click;
+            // 
+            // btnSaveConnection
+            // 
+            btnSaveConnection.BorderRadius = 4;
+            btnSaveConnection.CustomizableEdges = customizableEdges21;
+            btnSaveConnection.DisabledState.BorderColor = Color.DarkGray;
+            btnSaveConnection.DisabledState.CustomBorderColor = Color.DarkGray;
+            btnSaveConnection.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            btnSaveConnection.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            btnSaveConnection.Font = new Font("Segoe UI", 9F);
+            btnSaveConnection.ForeColor = Color.White;
+            btnSaveConnection.Location = new Point(6, 333);
+            btnSaveConnection.Name = "btnSaveConnection";
+            btnSaveConnection.ShadowDecoration.CustomizableEdges = customizableEdges22;
+            btnSaveConnection.Size = new Size(198, 37);
+            btnSaveConnection.TabIndex = 3;
+            btnSaveConnection.Text = "Save Connection";
+            btnSaveConnection.Click += btnSaveConnection_Click;
+            // 
+            // btnLoadConnection
+            // 
+            btnLoadConnection.BorderRadius = 4;
+            btnLoadConnection.CustomizableEdges = customizableEdges23;
+            btnLoadConnection.DisabledState.BorderColor = Color.DarkGray;
+            btnLoadConnection.DisabledState.CustomBorderColor = Color.DarkGray;
+            btnLoadConnection.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            btnLoadConnection.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            btnLoadConnection.Font = new Font("Segoe UI", 9F);
+            btnLoadConnection.ForeColor = Color.White;
+            btnLoadConnection.Location = new Point(6, 290);
+            btnLoadConnection.Name = "btnLoadConnection";
+            btnLoadConnection.ShadowDecoration.CustomizableEdges = customizableEdges24;
+            btnLoadConnection.Size = new Size(198, 37);
+            btnLoadConnection.TabIndex = 2;
+            btnLoadConnection.Text = "Load Connection";
+            btnLoadConnection.Click += btnLoadConnection_Click;
+            // 
+            // btnNewConection
+            // 
+            btnNewConection.BorderRadius = 4;
+            btnNewConection.CustomizableEdges = customizableEdges25;
+            btnNewConection.DisabledState.BorderColor = Color.DarkGray;
+            btnNewConection.DisabledState.CustomBorderColor = Color.DarkGray;
+            btnNewConection.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            btnNewConection.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            btnNewConection.Font = new Font("Segoe UI", 9F);
+            btnNewConection.ForeColor = Color.White;
+            btnNewConection.Location = new Point(6, 247);
+            btnNewConection.Name = "btnNewConection";
+            btnNewConection.ShadowDecoration.CustomizableEdges = customizableEdges26;
+            btnNewConection.Size = new Size(198, 37);
+            btnNewConection.TabIndex = 1;
+            btnNewConection.Text = "New Connection";
+            btnNewConection.Click += btnNewConection_Click;
+            // 
+            // lstView
+            // 
+            lstView.Location = new Point(3, 3);
+            lstView.Name = "lstView";
+            lstView.Size = new Size(606, 238);
+            lstView.TabIndex = 0;
+            lstView.UseCompatibleStateImageBehavior = false;
+            // 
+            // tabModifiers
+            // 
+            tabModifiers.Controls.Add(guna2GroupBox1);
+            tabModifiers.Location = new Point(184, 4);
+            tabModifiers.Name = "tabModifiers";
+            tabModifiers.Padding = new Padding(3);
+            tabModifiers.Size = new Size(612, 442);
+            tabModifiers.TabIndex = 1;
+            tabModifiers.Text = "Modifiers / Hacks";
+            tabModifiers.UseVisualStyleBackColor = true;
+            // 
+            // tabSettings
+            // 
+            tabSettings.Location = new Point(184, 4);
+            tabSettings.Name = "tabSettings";
+            tabSettings.Size = new Size(612, 442);
+            tabSettings.TabIndex = 2;
+            tabSettings.Text = "Settings";
+            tabSettings.UseVisualStyleBackColor = true;
+            // 
+            // guna2GroupBox1
+            // 
+            guna2GroupBox1.CustomizableEdges = customizableEdges13;
+            guna2GroupBox1.Font = new Font("Segoe UI", 9F);
+            guna2GroupBox1.ForeColor = Color.FromArgb(125, 137, 149);
+            guna2GroupBox1.Location = new Point(0, 0);
+            guna2GroupBox1.Name = "guna2GroupBox1";
+            guna2GroupBox1.ShadowDecoration.CustomizableEdges = customizableEdges14;
+            guna2GroupBox1.Size = new Size(305, 229);
+            guna2GroupBox1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(guna2TabControl1);
+            Name = "Form1";
+            Text = "Form1";
+            guna2TabControl1.ResumeLayout(false);
+            tabFTPSettings.ResumeLayout(false);
+            tabModifiers.ResumeLayout(false);
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Guna.UI2.WinForms.Guna2TabControl guna2TabControl1;
+        private TabPage tabFTPSettings;
+        private TabPage tabModifiers;
+        private TabPage tabSettings;
+        private Guna.UI2.WinForms.Guna2Button btnNewConection;
+        private ListView lstView;
+        private Guna.UI2.WinForms.Guna2Button btnRoot;
+        private Guna.UI2.WinForms.Guna2Button btnDownload;
+        private Guna.UI2.WinForms.Guna2Button btnSaveConnection;
+        private Guna.UI2.WinForms.Guna2Button btnLoadConnection;
+        private Guna.UI2.WinForms.Guna2Button btnBack;
+        private Guna.UI2.WinForms.Guna2GroupBox guna2GroupBox1;
     }
 }
--- TomoEditor/Form1.cs
+++ TomoEditor/Form1.cs
@@ -6,5 +6,35 @@ namespace TomoEditor
         {
             InitializeComponent();
         }
+
+        private void btnNewConection_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLoadConnection_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSaveConnection_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDownload_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRoot_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
